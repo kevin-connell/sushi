b1994b337e17c35cfa3af802ef59dc0e864d6ee4
FileOtter <kevin.t.connell@gmail.com>
2020-10-09 15:54:49 -0400

    getting code ready for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,21 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Th1s1sn3w",
-  database: "sushi_db"
-});
+var connection;
 
-connection.connect(function(err) {
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Th1s1sn3w",
+    database: "sushi_db"
+  });
+};
+
+connection.connect(function (err) {
   if (err)
     throw err;
   console.log("Connected under id " + connection.threadId);
